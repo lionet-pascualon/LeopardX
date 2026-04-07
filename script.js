98df05ec20da28e56adf4b8ea7a58f4cb1e94f34
lionet-pascualon <miltonpascualon543@gmail.com>
2026-04-06 22:02:44 -0300

    Update script.js
    esto hace funcional los botones de filtros

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ document.addEventListener("keyup", e => {
     });
   }
 });
+
+
+function filtrar(categoria) {
+    const tarjetas = document.querySelectorAll('.card');
+    const botones = document.querySelectorAll('.btn-filtro');
+
+    // Cambiar el botón activo
+    botones.forEach(btn => {
+        btn.classList.remove('active');
+        if(btn.innerText.toLowerCase().includes(categoria)) {
+            btn.classList.add('active');
+        }
+    });
+
+    // Filtrar las tarjetas de los productos para filtrarlo al tocarlo
+    tarjetas.forEach(tarjeta => {
+        // Buscamos dentro de los 'tags' que pusimos antes
+        const etiquetas = tarjeta.querySelector('.tags').innerText.toLowerCase();
+        
+        if (categoria === 'todos' || etiquetas.includes(categoria)) {
+            tarjeta.style.display = "block"; // Mostrar
+        } else {
+            tarjeta.style.display = "none"; // Ocultar
+        }
+    });
+}
